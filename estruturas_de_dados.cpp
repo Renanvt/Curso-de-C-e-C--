0421f0aaf4254a19c5f6f346a511797d49a597a1
Renanvt <jonatan_rvs@hotmail.com>
2020-05-14 15:54:09 -0300

    definindo regionalização

---
+++ estruturas_de_dados.cpp
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+#include <locale.h> //Essa biblioteca possui funушes de regionalizaусo
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
+#include <stdbool.h>
+#include <conio.h>
+#include <iostream>
+#define TAM 4
+using namespace std;
+
+int main(){
+    setlocale(LC_ALL,"");
+    return 0;
+}
---
+++ regionalizacao_ptbr_c++.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <locale.h> //Essa biblioteca possui funções de regionalização
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
+#include <stdbool.h>
+#include <conio.h>
+#include <iostream>
+#define TAM 4
+using namespace std;
+
+int main(){
+    setlocale(LC_ALL,"");
+    return 0;
+}
