49ee511f646f2c42c43eb640a4c490f0f3485879
Renanvt <jonatan_rvs@hotmail.com>
2020-07-06 04:12:50 -0300

    Private

---
+++ POO_3.cpp
@@ -0,0 +1,8 @@
+﻿#include <iostream>
+#include "classes.h"
+using namespace std;
+int main(){
+    Veiculo *v1 = new Veiculo(1);
+    cout << v1->;
+    return 0;
+}
---
+++ classes.h
@@ -0,0 +1,27 @@
+﻿using namespace std;
+class Veiculo{
+    //As propriedades public e privados só podem ser manipulados dentro da própria classe
+    public:
+        int vel;
+        int tipo;
+        Veiculo(int tp);//Construtor
+    private:
+        string nome;
+        int velMax;
+        bool ligado;
+};
+
+Veiculo::Veiculo(int tp){
+    // 1 = Carro, 2 = Aviao, 3=Navio
+    tipo=tp;
+    if(tipo==1){
+        nome="Carro";
+        velMax=200;
+    }else if(tipo==2){
+        nome="Aviao";
+        velMax=800;
+    }else if(tipo==3){
+        nome="Navio";
+        velMax=120;
+    }
+}
