89011e57e79cc786a65678b6c738695a6738ac1d
Renanvt <jonatan_rvs@hotmail.com>
2020-07-06 04:01:52 -0300

    Construtores

---
+++ Aviao.h
@@ -0,0 +1,36 @@
+﻿//#ifndef AVIAO_H_INCLUDED
+//#define AVIAO_H_INCLUDED
+using namespace std;
+#include <iostream>
+class Aviao{
+    public:
+        
+        int vel=0;
+        int velMax;
+        string tipo;
+        Aviao(int tp); //Prototipando o Construtor
+        void Imprimir();
+    private:
+};
+
+Aviao::Aviao(int tp){//Declaracao do Metodo Construtor
+    if(tp==1){
+        tipo="Jato";
+        velMax=800;
+    }else if(tp==2){
+        tipo="Monomotor";
+        velMax=350;
+    }else if(tp==3){
+        tipo="Planador";
+        velMax=180;
+    }
+}
+void Aviao::Imprimir(){
+    cout << "---------------------------" << endl;
+    cout<< "Tipo............: " << tipo << endl;
+    cout<< "Velocidade maxima: " << velMax << endl;
+    cout<< "Velocidade atual.: " << vel << endl;
+    cout << "---------------------------" << endl;
+
+}
+//#endif
